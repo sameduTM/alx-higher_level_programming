4642a0ba3002e8ecf8166ec32fa09f393663be44
Ken Wekesa <wanyamak884@gmail.com>
2023-10-04 02:58:04 +0300

    print string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            upper_char = chr(ord(char) - ord('a') + ord('A'))
+            result += upper_char
+        else:
+            result += char
+    print(result)
