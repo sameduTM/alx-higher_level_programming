ce110bf173b3a27ea68d67ebc1b0f4558d609cd9
Ken Wekesa <wanyamak884@gmail.com>
2023-10-04 02:34:57 +0300

    removed comma from last integer

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for first_digit in range(10):
-    for second_digit in range(first_digit + 1, 10):
-        if first_digit != second_digit:
-            print("{:02d}, ".format(first_digit * 10 + second_digit), end='')
-
+for r in range(0, 89):
+    if r % 10 > r // 10:
+        print("{:02}".format(r), ", ", sep='', end='')
+print("89")
