8c5e25ee7cc02dd169f23b50ad582f874a92aab7
Ken Wekesa <wanyamak884@gmail.com>
2023-10-03 16:49:48 +0300

    changed conditional statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 if (number < 0):
-    last_digit = (-number) % 10
+    last_digit = (number) % 10
+    last_digit = last_digit * -1
 else:
     last_digit = number % 10
-c = (f'Last digit of {number} is -{last_digit} and is less than 6 and not 0')
+c = (f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
 if last_digit < 6 and last_digit != 0:
     print(c)
 elif last_digit > 5:
