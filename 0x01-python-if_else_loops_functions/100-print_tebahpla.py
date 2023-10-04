462ef11d035a9c99c2d2d0e99dab2eeb25eec9a4
Ken Wekesa <wanyamak884@gmail.com>
2023-10-04 04:36:18 +0300

    used .format()

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('a') - 1, -1):
     if char % 2 == 0:
-        print(chr(char).lower(), end='')
+        print("{}".format(chr(char).lower()), end='')
     else:
-        print(chr(char).upper(), end='')
+        print("{}".format(chr(char).upper()), end='')
