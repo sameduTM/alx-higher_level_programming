40dec78199e10bd58b8a0c73cb656cd63c17b886
Ken Wekesa <wanyamak884@gmail.com>
2023-10-07 00:39:29 +0300

    check for 0 arguments

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,9 +3,7 @@ import sys
 from calculator_1 import add, sub, mul, div
 x = sys.argv
 if __name__ == "__main__":
-    if len(x) < 2:
-        exit(1)
-    if len(x) != 4:
+    if len(x) < 2 or len(x) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(x[1])
