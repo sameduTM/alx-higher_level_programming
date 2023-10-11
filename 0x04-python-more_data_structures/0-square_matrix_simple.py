f4453647c1dc967adbbb7c35d28e1ae8bbe39f10
Ken Wekesa <wanyamak884@gmail.com>
2023-10-11 17:37:52 +0300

    square value of all integers

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+   res_matrix = [[0 for x in range(len(matrix[0]))] for x in range(len(matrix))]
+
+   for i in range(len(matrix)):
+       for j in range(len(matrix[0])):
+           res_matrix[i][j] = matrix[i][j] ** 2
+   return res_matrix
