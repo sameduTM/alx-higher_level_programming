67e4bd63963a3c9a58e1fd7a5886db39104a29a2
Ken Wekesa <wanyamak884@gmail.com>
2023-10-11 17:42:36 +0300

    pycodestyle check

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    res_matrix = [[0 for x in range(len(matrix[0]))] for x in range(len(matrix))]
+    res_mat = [[0 for x in range(len(matrix[0]))] for x in range(len(matrix))]
 
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            res_matrix[i][j] = matrix[i][j] ** 2
+            res_mat[i][j] = matrix[i][j] ** 2
 
-    return res_matrix
+    return res_mat
