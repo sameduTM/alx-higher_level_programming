1ce6747f85b5fdafbe713a8728c5d311d83778c3
Ken Wekesa <wanyamak884@gmail.com>
2023-10-12 13:46:09 +0300

    fixed pycodestyle

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -8,7 +8,7 @@ def weight_average(my_list=[]):
 
     for score, weight in my_list:
         total_weighted_sum += score * weight
-        total_weight +=  weight
+        total_weight += weight
 
     if total_weight == 0:
         return 0
