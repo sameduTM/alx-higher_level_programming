7c4c58e2896fcbde064289fbed90e90603e05c08
Ken Wekesa <wanyamak884@gmail.com>
2023-10-24 01:46:32 +0300

    divides element by element 2 lists.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result = []
-
-    try:
-        for i in range(list_length):
-            if i < len(my_list_1) and i < len(my_list_2):
-                element1 = my_list_1[i]
-                element2 = my_list_2[i]
-
-                if isinstance(element1, (int, float)) and isinstance(
-                    element2, (int, float)
-                ):
-                    if element2 != 0:
-                        result.append(element1 / element2)
-                    else:
-                        print("division by 0")
-                        result.append(0)
-                else:
-                    print("wrong type")
-                    result.append(0)
+    new_list = []
+    for i in range(list_length):
+        try:
+            if isinstance(my_list_1[i], (int, float)) and isinstance(
+                my_list_2[i], (int, float)
+            ):
+                new_list.append(my_list_1[i] / my_list_2[i])
             else:
-                print("out of range")
-                result.append(0)
-    except Exception as e:
-        print("An unexpected error occurred:", e)
-
-    return result
+                print("wrong type")
+                new_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+    return new_list
