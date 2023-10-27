54efc3a620e71cbe45759704f4494aca3835f0da
Ken Wekesa <wanyamak884@gmail.com>
2023-10-28 02:51:57 +0300

    check test cases

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list_copy = []
-    list_copy = my_list
+    new_list = [0] * len(my_list)
     if idx < 0 or idx > len(my_list):
-        return list_copy
-    else:
-        my_list[idx] = element
         return my_list
+    for i in range(len(my_list)):
+        new_list[i] = my_list[i]
+    new_list.insert(idx, element)
+    new_list.pop(idx + 1)
+    return new_list
