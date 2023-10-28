ccd9634572e9cb1e48419e57742fd604d39d430d
Ken Wekesa <wanyamak884@gmail.com>
2023-10-28 15:44:54 +0300

    2 test cases fail check

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
         y = len(matrix[i])
         for j in range(y):
             print("{:d}".format(matrix[i][j]), end="")
-            if j == x - 1:
+            if j == y - 1:
                 continue
             print(end=" ")
         print()
