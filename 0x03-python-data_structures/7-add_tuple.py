e9c5aff59ba5130aee5fa5468380b47eb5f43b9c
Ken Wekesa <wanyamak884@gmail.com>
2023-10-28 23:47:29 +0300

    check 3 test cases

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,8 +10,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) == 0:
         tuple_b = (0, 0)
     if len(tuple_a) > 2 or len(tuple_b) > 2:
-        tuple_a = (0, 0)
-        tuple_b = (0, 0)
+        tuple_a = (tuple_a[0], tuple_a[1])
+        tuple_b = (tuple_b[0], tuple_b[1])
     for i in range(1):
         tup_c = (tuple_a[i] + tuple_b[i], tuple_a[i + 1] + tuple_b[i + 1])
     return tup_c
