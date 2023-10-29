e65b3038e96556c779e2f0615e42131dd60990df
Ken Wekesa <wanyamak884@gmail.com>
2023-10-29 15:28:03 +0300

    removed the max function

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if len(my_list) == 0:
         return None
-    return max(my_list)
+    max_val = my_list[0]
+
+    for i in range(1, len(my_list)):
+        if my_list[i] > max_val:
+            max_val = my_list[i]
+    return max_val
