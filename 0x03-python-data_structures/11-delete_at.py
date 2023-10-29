56d95da0738a93636fa0834ba2c68441d840d75d
Ken Wekesa <wanyamak884@gmail.com>
2023-10-29 22:12:43 +0300

    switch between two vars in py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list = []
-    x = len(my_list)
-    if idx < 0 or idx >= x:
-        return my_list
-    for i in range(x):
-        if i != idx:
-            new_list.append(my_list[i])
     del my_list[idx]
-    return new_list
+    return my_list
