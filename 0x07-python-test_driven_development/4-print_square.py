8f4f45d90c6fe3671d893a6d73fafb54a6aa2a3b
Ken Wekesa <wanyamak884@gmail.com>
2023-11-04 04:29:00 +0300

    check test cases for square print module

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is the ``square`` module that prints a square
+
+The module has only one function, print_square
+"""
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size < 0 and isinstance(size, float):
+        raise TypeError("size must be an integer")
+    else:
+        for i in range(size):
+            for j in range(size):
+                print("#", end="")
+            print()
