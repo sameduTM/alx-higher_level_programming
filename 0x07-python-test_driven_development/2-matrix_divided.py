d5f9c838c9c2a042a2a3f71e1c7033a0c7cf0814
Ken Wekesa <wanyamak884@gmail.com>
2023-11-05 20:10:06 +0300

    first commit task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""
-This is the "matrix division" module
-
-This module only has 1 function, matrix_divided
-"""
+"""This is ``matrix division`` module"""
 
 
 def matrix_divided(matrix, div):
-    """Function divides all elements of a matrix"""
-    new_matrix = []
+    """This is the matrix division function"""
+    if not all(
+        isinstance(row, list) and all(
+            isinstance(element, (int, float)) for element in row
+            ) for row in matrix):
+        if len(set(len(row) for row in matrix)) != 1:
+            raise TypeError("Each row of the matrix must have the same size")
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    for row in matrix:
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        for i in row:
-            if not isinstance(i, int):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            new_matrix.append(i / div)
 
-    print(len(row))
+    new_matrix = [[round(elem / div, 2) for elem in row] for row in matrix]
 
     return new_matrix
