f715c16d0e81847cf249232dca6d05fbe761b859
Ken Wekesa <wanyamak884@gmail.com>
2023-11-06 12:39:56 +0300

    task 1 first commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is a simple square module"""
+
+
+class Square:
+    """This module has instance attribute size"""
+    def __init__(self, size):
+        self.__size = size
