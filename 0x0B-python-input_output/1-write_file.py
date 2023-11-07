4f1b3f98a71222460e9be452c3268b2cafb248d6
Ken Wekesa <wanyamak884@gmail.com>
2023-11-08 01:21:27 +0300

    documented my code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This is the ``write to file`` module"""
+
+
 def write_file(filename="", text=""):
+    """This is the only write to file function in module"""
     with open(filename, mode="w", encoding="UTF8") as f:
         w = f.write(text)
         return w
