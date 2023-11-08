0c52a346df8e84b0d5e419a4120d2ebafd224035
Ken Wekesa <wanyamak884@gmail.com>
2023-11-08 16:24:46 +0300

    private import

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """This is ``from json to object`` module"""
 
 
 def from_json_string(my_str):
+    import json
     """from_json_string to obj function"""
     return json.loads(my_str)
