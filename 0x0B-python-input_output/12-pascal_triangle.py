37778c755c004a93857e7eb703c20aab5476efc5
Ken Wekesa <wanyamak884@gmail.com>
2023-11-09 01:48:03 +0300

    pascal triangle code

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,7 +4,20 @@
 
 def pascal_triangle(n):
     """This module only has one function, pascal_triangle"""
-    my_list = []
     if n <= 0:
-        return 0
-    
+        return []
+
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+        else:
+            # Calculate the next row based on the previous row
+            previous_row = triangle[i - 1]
+            new_row = [1]
+            for j in range(1, i):
+                new_row.append(previous_row[j - 1] + previous_row[j])
+            new_row.append(1)
+            triangle.append(new_row)
+
+    return triangle
