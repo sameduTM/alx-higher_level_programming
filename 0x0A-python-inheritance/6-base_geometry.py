119a2e6b80bac18f6e840730bbe741b3ad48c1dc
Ken Wekesa <wanyamak884@gmail.com>
2023-11-09 04:04:24 +0300

    Improve geometry -public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is the ``improve geometry`` module
+"""
+
+
+class BaseGeometry:
+    """This is the only class of the module"""
+    def area(self):
+        """Public instance method, area"""
+        raise Exception("area() is not implemented")
