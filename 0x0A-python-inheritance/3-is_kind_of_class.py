d43173a2aff414465acac66c61714175a2c52218
Ken Wekesa <wanyamak884@gmail.com>
2023-11-10 17:03:11 +0300

    same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""This is the `` Same class or inherit from`` module
-"""
+"""3. Same class or inherit from module"""
 
 
 def is_kind_of_class(obj, a_class):
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class):
+        return True
