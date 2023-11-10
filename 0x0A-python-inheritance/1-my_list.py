59db90df92c976454f6de8c58abcae5ded7152b7
Ken Wekesa <wanyamak884@gmail.com>
2023-11-10 16:33:17 +0300

    use return instead of print

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -24,5 +24,5 @@ class MyList(list):
         if all(isinstance(element, int) for element in self):
             sorted_list = self.copy()
             sorted_list.sort()
-            print(sorted_list)
+            # print(sorted_list)
             return sorted_list
