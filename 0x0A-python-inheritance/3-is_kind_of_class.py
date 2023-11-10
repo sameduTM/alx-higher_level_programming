34ac0e79eae001dc997ef76f2cb41b7b05ebfdab
Ken Wekesa <wanyamak884@gmail.com>
2023-11-10 17:06:12 +0300

    commented the function

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,5 +3,8 @@
 
 
 def is_kind_of_class(obj, a_class):
+    """function checks f the object is an instance of,
+    or if the object is an instance of a class that inherited from
+    """
     if isinstance(obj, a_class):
         return True
