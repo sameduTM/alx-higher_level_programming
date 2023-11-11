06bfd2de8b38efa12142f1bb590634a812293086
Ken Wekesa <wanyamak884@gmail.com>
2023-11-11 03:42:46 +0300

    only subclass of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is the ``Only sub class of`` module"""
+
+
+def inherits_from(obj, a_class):
+    """This is ``inherits from`` function"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
