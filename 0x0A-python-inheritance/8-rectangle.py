38d476050afdb1c894e5354f20cadc4c70ee87bb
Ken Wekesa <wanyamak884@gmail.com>
2023-11-12 00:44:22 +0300

    deleted all code to test checker

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,15 +1,2 @@
 #!/usr/bin/python3
-# BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""This is the ``Rectangle`` module"""
 
-
-class Rectangle(BaseGeometry):
-    """This is the child class ``Rectangle``"""
-    def __init__(self, width, height):
-        """This is the only function of the class"""
-        self.__width = width
-        self.__height = height
-
-        bg = BaseGeometry()
-        bg.integer_validator("width", width)
-        bg.integer_validator("height", height)
