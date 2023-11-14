1506dde5baf0829bc213feda18e65ff8cedf8003
Ken Wekesa <wanyamak884@gmail.com>
2023-11-14 03:34:21 +0300

    push to task 10

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This is the ``Base class`` module"""
+
+
+class Base:
+    """This is the Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from models.rectangle import rectangle
+"""The is square module"""
+
+
+class Square(Rectangle):
+    """This is the class Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(id)
+        super().__init__(x)
+        super().__init__(y)
+        super().__init__(width)
+        super().__init__(height)
+
