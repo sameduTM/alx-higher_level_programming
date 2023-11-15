9b1c9e1e205cb80b748a89818f0450ae82523fc3
Ken Wekesa <wanyamak884@gmail.com>
2023-11-16 00:21:07 +0300

    returned empty list instead of string []

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,7 +35,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         if json_string is None:
-            return "[]"
+            return []
         else:
             return json.loads(json_string)
 
