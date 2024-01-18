59c018fbd41721004acee9e13ff6a78f3b00d6b6
Ken Wekesa <wanyamak884@gmail.com>
2024-01-18 13:03:52 +0300

    used SHOW CREATE instead

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Write a script that prints the full description of the table
 -- first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
