1db0e1b53ce21b424a177c2c870c0d5277e8d3d5
Ken Wekesa <wanyamak884@gmail.com>
2024-02-01 12:13:05 +0000

    task 3 check

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const checkAgs = () => {
+    const numArgs = process.argv.length - 2;
+
+    if (numArgs === 0) {
+        console.log('No argument');
+    } else if (numArgs === 1) {
+        console.log('Argument found');
+    } else {
+        console.log('Arguments found');
+    }
+};
+
+checkAgs();
