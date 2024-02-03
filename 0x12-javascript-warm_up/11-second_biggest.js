3f3f98f38dcfb595644d2a3fd7edf6411369ab94
Ken Wekesa <wanyamak884@gmail.com>
2024-02-03 11:32:12 +0000

    correct output, task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -12,7 +12,6 @@ const secondL = () => {
       arr.push(parseInt(process.argv[i + 2]));
       i++;
     }
-    console.log(arr);
     for (let i = 0; i < arr.length - 1; i++) {
       for (let j = 0; j < arr.length - 1; j++) {
         if (arr[j] < arr[j + 1]) {
