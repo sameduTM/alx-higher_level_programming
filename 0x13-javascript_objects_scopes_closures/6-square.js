2d53efcb12d895d20d63fee46bbc003e887140d6
Ken Wekesa <wanyamak884@gmail.com>
2024-02-12 19:17:32 +0000

    task 7

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,6 +2,18 @@
 const Square5 = require('./5-square');
 
 module.exports = class Square extends Square5 {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str += c;
+      }
+      console.log(str);
+    }
+  }
+};
+/*
+module.exports = class Square extends Square5 {
   constructor (size) {
     super(size, size);
   }
@@ -12,3 +24,4 @@ module.exports = class Square extends Square5 {
     }
   }
 };
+*/
