3d7e525afe34dfba7a5aa3d350027793b7340c67
Ken Wekesa <wanyamak884@gmail.com>
2024-02-12 22:13:29 +0000

    change spelling

--- 0x13-javascript_objects_scopes_closures/7-main.js
+++ 0x13-javascript_objects_scopes_closures/7-main.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const nbOccurrences = require('./7-occurrences').nbOccurrences;
+const nbOccurences = require('./7-occurrences').nbOccurences;
 
-console.log(nbOccurrences([1, 2, 3, 4, 5, 6], 3));
+console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
 // console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
 // console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
