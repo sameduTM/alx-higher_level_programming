23ee30547882556b4696505c04876654d14e3841
Ken Wekesa <wanyamak884@gmail.com>
2024-02-20 14:02:55 +0300

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,18 +2,16 @@
 
 const { dict } = require('./101-data.js');
 
-let sorted = {};
+const sorted = {};
 
-for (let key in dict) {
-    if (dict.hasOwnProperty(key)) {
-        const value = dict[key];
-        if (!sorted.hasOwnProperty(value)) {
-            sorted[value] = [];
-        }
-        sorted[value].push(key);
+for (const key in dict) {
+  if (dict.hasOwnProperty(key)) {
+    const value = dict[key];
+    if (!sorted.hasOwnProperty(value)) {
+      sorted[value] = [];
     }
+    sorted[value].push(key);
+  }
 }
 
 console.log(sorted);
-
-
