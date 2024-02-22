23f3904b19d8426300aba441688e9935483910b7
Ken Wekesa <wanyamak884@gmail.com>
2024-02-22 11:00:06 +0000

    task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,11 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const file1 = process.argv[2];
 const file2 = process.argv[3];
+const file3 = process.argv[4];
 
 const fs = require('node:fs');
 
-const fl1 = fs.readFileSync('fileA', { encoding: 'utf8', flag: 'r' });
-const fl2 = fs.readFileSync('fileB', { encoding: 'utf8', flag: 'r' });
+const fl1 = fs.readFileSync(file1, { encoding: 'utf8', flag: 'r' });
+const fl2 = fs.readFileSync(file2, { encoding: 'utf8', flag: 'r' });
 
-fs.appendFileSync('fileC', fl1 + fl2);
+fs.appendFileSync(file3, fl1 + fl2);
