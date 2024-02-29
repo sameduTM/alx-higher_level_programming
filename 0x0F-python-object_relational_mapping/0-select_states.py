9c76898345eff7f1f99af0c35317fcf236ee3b66
Ken Wekesa <wanyamak884@gmail.com>
2024-02-29 22:27:49 +0300

    included a function and added documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""
-    This is a simple MySQLdb script that lists all states in table
+import MySQLdb
+import sys
+"""This is a simple MySQLdb script that lists all states in table
     in the database
 """
 
-import MySQLdb
-import sys
 
-i = sys.argv[1]
-j = sys.argv[2]
-k = sys.argv[3]
+def states():
+    """This is a simple function that lists all states in a table"""
+    i = sys.argv[1]
+    j = sys.argv[2]
+    k = sys.argv[3]
 
-db = MySQLdb.connect(host="localhost", user=i, passwd=j, db=k)
+    db = MySQLdb.connect(host="localhost", user=i, passwd=j, db=k)
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
 
-rows = cur.fetchall()
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
+
+
+if __name__ == '__main__':
+    states()
