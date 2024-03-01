7fea37e3683cd0538f626887b7b0c79ad6c8b78a
Ken Wekesa <wanyamak884@gmail.com>
2024-03-01 16:07:27 +0300

    pycodestyle check

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,8 @@ def state():
         db=sys.argv[3], port=3306
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(sys.argv[4],))
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(sys.argv[4],))
 
     rows = cur.fetchall()
 
