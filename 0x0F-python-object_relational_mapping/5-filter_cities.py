637c25757141c5cb83156c54d31a90c34b4cceb3
Ken Wekesa <wanyamak884@gmail.com>
2024-03-04 10:56:16 +0300

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""a script that takes in the name of a state as an
+argument and lists all cities of that state,
+using the database"""
+import MySQLdb
+import sys
+
+
+def city():
+    """This is only function of module"""
+    usrname = sys.argv[1]
+    passwrd = sys.argv[2]
+    dtbase = sys.argv[3]
+    argc = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', user=usrname,
+                         passwd=passwrd, db=dtbase, port=3306)
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.name FROM cities\
+        INNER JOIN states ON\
+        cities.state_id = states.id WHERE states.name = %s", (argc,))
+
+    rows = cur.fetchall()
+    """lst = []
+
+    for row in rows:
+        lst += row
+
+    for i in range(len(lst)):
+        print(lst[i], end='')
+        if (i != len(lst) - 1):
+            print(", ", end='')
+    print("\n")"""
+    city_names = [row[0] for row in rows]
+    print(", ".join(city_names))
+
+
+if __name__ == '__main__':
+    city()
