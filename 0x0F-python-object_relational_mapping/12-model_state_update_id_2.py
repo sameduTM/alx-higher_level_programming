9481ac2bb8f1c418963d947c8f0e8866e02f53cb
Ken Wekesa <wanyamak884@gmail.com>
2024-03-04 19:42:04 +0300

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that changes the name of a State object
+from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, text
+
+
+def state():
+    """This is the only function of script"""
+    # create a connection
+    with engine.connect() as conn:
+        # create a transaction
+        trans = conn.begin()
+        conn.execute(text("UPDATE states SET name='New Mexico' WHERE id=2"))
+
+        trans.commit()
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    state()
