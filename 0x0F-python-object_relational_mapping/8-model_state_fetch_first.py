6a40bcc343ca5dd2e2f9e996ce3163f36be228c6
Ken Wekesa <wanyamak884@gmail.com>
2024-03-04 14:12:47 +0300

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine, text
+
+
+def state():
+    """This is the only function in the module"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from states order by states.id"))
+
+    rows = result.fetchone()
+
+    print(str(rows.id) + ":", rows.name)
+
+
+if __name__ == "__main__":
+    state()
