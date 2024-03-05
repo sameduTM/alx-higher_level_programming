02412b0dd3f54f5805224b89a9487182eb8dfeb7
Ken Wekesa <wanyamak884@gmail.com>
2024-03-06 02:04:24 +0300

    included metadate

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -3,9 +3,9 @@
 contained in the database hbtn_0e_101_usa
 """
 import sys
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
+from relationship_state import State, Base
 from relationship_city import City
 
 
@@ -13,6 +13,8 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
+    metadata = MetaData()
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).order_by(State.id).all()
