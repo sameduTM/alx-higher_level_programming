24303e89a73f70c45ddad75bd4345776c0768e07
Ken Wekesa <wanyamak884@gmail.com>
2024-05-25 00:00:32 +0300

    sends GET request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-if eval "curl -sI $1" | grep -q 'HTTP/1.1 200 OK'; then curl -s -X "GET" $1; fi
+curl -L $1
