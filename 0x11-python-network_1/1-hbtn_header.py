9c963c782f6cfab84f6f7143fa5a8a2d20c5d98d
Ken Wekesa <wanyamak884@gmail.com>
2024-05-30 23:45:52 +0300

    documented the script

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from urllib.request import urlopen
 import sys
+"""Python3 script that displays the value of the X-Request-Id variable found
+in the header of the response.
+"""
 
 with urlopen(sys.argv[1]) as response:
     print(response.headers['X-Request-Id'])
