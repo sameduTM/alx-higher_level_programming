f40dbdad54b4bb46fa8f860d201c3ae4d8703f21
Ken Wekesa <wanyamak884@gmail.com>
2024-05-30 19:56:14 +0300

    Whats my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+from urllib.request import urlopen
+# from urllib.parse import urlencode
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urlopen(url) as response:
+    body = response.read()
+    char_set = response.headers.get_content_charset()
+
+print("Body responses:")
+print("    - type: ", type(body))
+print("    - content: ", body)
+print("    - utf8 content: ", body.decode(char_set))
