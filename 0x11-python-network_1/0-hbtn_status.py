344377d252bdb9fb9970aa6f329022ff8bb70e1c
Ken Wekesa <wanyamak884@gmail.com>
2024-05-30 20:13:06 +0300

    :spelkling check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ with urlopen(url) as response:
     body = response.read()
     char_set = response.headers.get_content_charset()
 
-print("Body responses:")
+print("Body response:")
 print("- type:", type(body))
 print("- content:", body)
 print("- utf8 content:", body.decode(char_set))
