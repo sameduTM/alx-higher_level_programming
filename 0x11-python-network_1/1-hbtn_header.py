5064c3ae62fd6cdaacd2408ba76c16efc1393a0b
Ken Wekesa <wanyamak884@gmail.com>
2024-05-30 23:32:06 +0300

    response header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id variable found in the header of
+the response."""
+from urllib.request import urlopen
+import sys
+
+with urlopen(sys.argv[1]) as response:
+    print(response.headers['X-Request-Id'])
