abaaf4cbabeb331a7cc8350ddc442951f2965ded
Ken Wekesa <wanyamak884@gmail.com>
2024-06-02 19:04:52 +0300

    response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 from urllib.request import urlopen
 import sys
-"""Python script that displays the value of the X-Request-Id variable found
-in the header of the response.
+"""Python script that displays the value of the X-Request-Id variable found in the header of the response.
 """
 
 with urlopen(sys.argv[1]) as response:
