6b7cac6e13c2f6c4b3aafa7af3e7006835929178
Ken Wekesa <wanyamak884@gmail.com>
2024-06-12 11:16:55 +0300

    not imported when run

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,18 +5,24 @@ http://0.0.0.0:5000/search_user with the letter as a parameter.
 import requests
 import sys
 
-url = "http://0.0.0.0:5000/search_user"
-if len(sys.argv) == 1:
-    q = ""
-else:
-    q = sys.argv[1]
-r = requests.post(url, data={'q': q})
 
-try:
-    body = r.json()
-    if len(body) > 0:
-        print(f"[{body['id']}] {body['name']}")
+def search_api(url):
+    if len(sys.argv) == 1:
+        q = ""
     else:
-        print("No result")
-except TypeError:
-    print("Not a valid JSON")
+        q = sys.argv[1]
+    r = requests.post(url, data={'q': q})
+
+    try:
+        body = r.json()
+        if len(body) > 0:
+            print(f"[{body['id']}] {body['name']}")
+        else:
+            print("No result")
+    except TypeError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    search_api(url)
