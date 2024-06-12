47b2c6f2147d38df9b5afb7957a9218e95b5b7cf
Ken Wekesa <wanyamak884@gmail.com>
2024-06-12 12:42:35 +0300

    error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""script that takes in a URL, sends a request to the URL and displays the body of the response."""
+"""script that takes in a URL, sends a request to the URL and displays the
+body of the response.
+"""
 import requests
 import sys
 
-r = requests.get(sys.argv[1])
 
-if r.status_code >= 400:
-    print(f"Error code: {r.status_code}")
-else:
-    print(r.text)
+def error_code(url):
+    r = requests.get(url)
+
+    if r.status_code >= 400:
+        print(f"Error code: {r.status_code}")
+    else:
+        print(r.text)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    error_code(url)
